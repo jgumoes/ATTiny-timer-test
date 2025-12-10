4975a444c90c179c2f3a23b70b04aa41b7728619
Jack Gumoes <j.gumoes@gmail.com>
2025-12-10 21:12:15 +0000

    longer delay on timer0 - uses interrupt to multiply the shorter interval into longer ones

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <Arduino.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+uint8_t delayCounter = 0;
+
+ISR(TIMER0_COMPA_vect){
+  delayCounter++;
+}
 
 void setup() {
 
   DDRB = (1<<PB3) | (1<<PB4); // set pins as outputs
   
-  // TIMSK = bit(OCIE0A) | bit(OCIE1A);  // enable interrupts on output compare registers A
+  TIMSK = bit(OCIE0A);  // enable timer0 interrupt
+  SREG |= bit(SREG_I);  // enable global interrupts
   
   /*
   timer0 setup:
     - CLK/1024 prescale
-    - 204.6 mS pulse width
+    - 102 mS pulse width
     - TODO: attach interrupt to get to ~1 S
   */
   {
-    TCCR0A = 0;  // set mode to normal
-    OCR0A = (uint8_t)200; // 8MHz * TCCR0B * (T_uS/2)
+    TCCR0A = bit(WGM01);  // set mode to CTC
+    OCR0A = (uint8_t)100; // 8MHz * TCCR0B * (T_uS/2)
     TCCR0B = bit(CS02) | bit(CS00);  // clk/1024
   }
 
@@ -34,10 +42,11 @@ void setup() {
 }
 
 void loop() {
-  if(TIFR & bit(OCF0A)){  // if timer0 has matched OCR0A
+
+  if(delayCounter >= 11){  // should be ~1.1 second
     PINB = bit(PB3);  // toggle PB3
     TCNT0 = 0;  // reset the timer
-    TIFR = bit(OCF0A);  // reset the flag
+    delayCounter = 0;
   }
 
   if(TIFR & bit(OCF1A)){  // if timer1 has matched OCR0A
