b3593dc769454ff8cefd3d3406026dd86018bb34
Jack Gumoes <j.gumoes@gmail.com>
2025-12-11 18:02:02 +0000

    pins are set instead of toggled - sets pins through PORTB instead of PINB - pin state is deterministic

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ ISR(TIMER0_COMPA_vect){
   delayCounter++;
 }
 
+uint8_t state3 = 0; // state of PB3
+uint8_t state4 = 0; // state of PB4
+
 void setup() {
 
   DDRB = (1<<PB3) | (1<<PB4); // set pins as outputs
@@ -44,13 +47,45 @@ void setup() {
 void loop() {
 
   if(delayCounter >= 11){  // should be ~1.1 second
-    PINB = bit(PB3);  // toggle PB3
+    // PINB = bit(PB3);  // toggle PB3
+    switch (state3)
+    {
+    case 0:
+      state3 = 1;
+      PORTB |= bit(PB3);
+      break;
+    
+    case 1:
+      state3 = 0;
+      PORTB &= ~bit(PB3);
+      break;
+
+    default:
+      state3 = 0;
+      break;
+    }
     TCNT0 = 0;  // reset the timer
     delayCounter = 0;
   }
 
   if(TIFR & bit(OCF1A)){  // if timer1 has matched OCR0A
-    PINB = bit(PB4);  // toggle PB4
+    // PINB = bit(PB4);  // toggle PB4
+    switch (state4)
+    {
+    case 0:
+      state4 = 1;
+      PORTB |= bit(PB4);
+      break;
+    
+    case 1:
+      state4 = 0;
+      PORTB &= ~bit(PB4);
+      break;
+      
+    default:
+      state4 = 0;
+      break;
+    }
     TCNT1 = 0;  // reset the timer
     TIFR = bit(OCF1A);  // reset the flag
   }
